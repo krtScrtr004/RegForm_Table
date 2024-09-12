4a18cef86980d6f5d10e7c385142354e1a265117
Kurt Secretario <secretariokurt3219@gmail.com>
2024-09-12 16:11:47 +0800

    add birth date validation function

--- script/index.js
+++ script/index.js
@@ -22,6 +22,7 @@ function validateFname(NAME) {
     }
 }
 
+// Last name validation
 function validateLname(NAME) {
     const MIN = 1, MAX = 100;
     if (NAME.length < MIN || NAME.length > MAX) {
@@ -30,5 +31,34 @@ function validateLname(NAME) {
 
     if (allowedNameChars.test(NAME)) {
         errorMsg += 'Last Name contains disallowed characters. \n';
+    }
+}
+
+// Birth date validation
+function validateBdate(DAY, MONTH, YEAR) {
+    const TODAY = new Date();
+    const MIN_YEAR = 1900, 
+          MAX_YEAR = TODAY.getFullYear();
+
+    // Check if the year is within the valid range
+    if (YEAR < MIN_YEAR || YEAR > MAX_YEAR) {
+        errorMsg += `Birth date year must be between ${MIN_YEAR} and ${MAX_YEAR}. \n`;
+    }
+
+    // Check if the month is valid
+    if (MONTH < 1 || MONTH > 12) {
+        errorMsg += 'Birth date month must be between 1 and 12. \n';
+    }
+
+    // Check if the day is valid for the given month and year
+    const DAYS_IN_MON = new Date(YEAR, MONTH, 0).getDate(); // Get the number of days in the month
+    if (DAY < 1 || DAY > DAYS_IN_MON) {
+        errorMsg += `Birth date day must be between 1 and ${DAYS_IN_MON} for the selected month and year. \n`;
+    }
+
+    // Check if the input date is in the future
+    const INPUT_DATE = new Date(YEAR, MONTH - 1, DAY);
+    if (INPUT_DATE > TODAY) {
+        errorMsg += 'Birth date cannot be in the future. \n';
     }
 }
